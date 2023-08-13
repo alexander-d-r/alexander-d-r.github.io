1f7a39673e15864a369595ec2a2de8024e405a0c
Daniel Russell Alexander <92579202+alexander-d-r@users.noreply.github.com>
2023-08-14 02:45:26 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,18 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// Function to get the current date and time
+function getCurrentDateAndTime() {
+  const dateTime = new Date();
+  return dateTime.toLocaleString();
+}
+
+// Target an HTML element to display the current date and time
+const dateDisplay = document.getElementById("date-container");
+
+// Set the innerHTML of the element to the current date and time returned by the function
+dateDisplay.innerHTML = getCurrentDateAndTime();
+
 (function($) {
 
 	var	$window = $(window),
